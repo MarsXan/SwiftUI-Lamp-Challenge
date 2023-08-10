18673dd3f3ba952d5228e2adb9a2f931c311c3e1
mohsen mokhtari <mr.marsxan@gmail.com>
2023-08-10 13:59:08 +0330

    Initial Commit

---
+++ LampChallenge/LampChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  LampChallengeApp.swift
+//  LampChallenge
+//
+//  Created by mohsen mokhtari on 8/10/23.
+//
+
+import SwiftUI
+
+@main
+struct LampChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
